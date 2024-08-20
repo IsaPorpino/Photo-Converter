852891d9183f5ceafd747ab1f4ef2d3a098cd06a
IsaPorpino <isabelasporpino@gmail.com>
2024-08-20 10:41:05 -0300

    Tratamento de repetição

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from functions import *
 def interface():
     st.title('Conversor de Imagens')
     options = ["-","PNG", "BMP", "JPEG"]
-    uploadedFile = st.file_uploader('Insira a sua imagem:', on_change=photoChange(uploadedFile, options))
+    uploadedFile = st.file_uploader('Insira a sua imagem:')
+
+    if uploadedFile is not None:
+        photoChange(uploadedFile, options)
 
 
 if __name__ == '__main__':
